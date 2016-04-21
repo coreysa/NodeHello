fbca4ea7b9128e0bf1fda55995ac17e98d113ed4
Corey Sanders <corey.sanders@microsoft.com>
2016-04-21 00:46:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Future decoded. My favourite colour is red.</h1>');
+    res.end('<h1>Hello DevDays. I love Sour drinks</h1>');
 }).listen(process.env.PORT);  
